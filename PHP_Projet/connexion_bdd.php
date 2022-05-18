72465a80fcf667bfe907ef9f9b42655d58ce57f8
Jwace <82574768+Jwace@users.noreply.github.com>
2022-05-18 21:25:58 +0200

    Delete PHP_Projet directory

--- PHP_Projet/connexion_bdd.php
+++
@@ -1,8 +0,0 @@
-<?php
-    try {
-        $dsn = 'mysql:host=localhost;dbname=sautre2u_bddphp;port=3306;charset=utf8';
-        $pdo =new PDO($dsn,"root","");
-    } catch(PDOException $exception) {
-        exit("Erreur de connexion à la base de données".$exception->getMessage());
-                            }
-?>
